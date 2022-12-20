80f3ca07de968851a463c863b7cf210049260a74
ecekpo <ememekpo@yahoo.com>
2022-12-20 13:00:43 +0100

    Add linters errors

--- base_decorator.rb
+++ base_decorator.rb
@@ -5,9 +5,9 @@ class BaseDecorator < Nameable
 
   def initialize(nameable)
     super()
-    @nameable = nameable 
-   end
-  
+    @nameable = nameable
+  end
+
   def correct_name
     @nameable.correct_name
   end
--- test.rb
+++ test.rb
@@ -3,10 +3,10 @@ require './capitalize_decorator'
 require './trimmer_decorator'
 
 person = Person.new(22, 'maximilianus')
-  person.correct_name
-  capitalizedPerson = CapitalizeDecorator.new(person)
-  capitalizedPerson.correct_name
-  capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
-  capitalizedTrimmedPerson.correct_name
+person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalizedPerson)
+capitalized_trimmed_person.correct_name
 
-#puts person.correct_name
+# puts person.correct_name
