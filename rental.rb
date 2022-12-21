a5f33a643d1b68fe7c4b4d1bfbeceea370ae7ff9
ecekpo <ememekpo@yahoo.com>
2022-12-21 11:40:25 +0100

    Add rentals belongs to a book

--- rental.rb
+++ rental.rb
@@ -1,7 +1,13 @@
 class Rental
-    attr_accessor :date
+    attr_accessor :date, :book, :person
   
-    def initialize(date)
+    def initialize(date, book, person)
      @data = date
+     @book = book
+     book.rental << self
+     @person = person
+     person.rental << self
     end
    end
+  
+  end
