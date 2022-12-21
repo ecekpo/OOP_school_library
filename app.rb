f1bdd2e0dc606dfa3ce21b1cd6ab32fb21752d6d
ecekpo <ememekpo@yahoo.com>
2022-12-21 14:18:07 +0100

    Add add to entry point

--- app.rb
+++ app.rb
@@ -19,4 +19,24 @@ class App
     puts '6 - List all rentals for a given person id'
     puts '7 - Exit'
    end
-   
+
+   def main
+    option = gets.chomp
+    case option
+    when '1'
+      list_all_books
+    when '2'
+    list_all_people
+    when '3'
+    create_a_person
+    when '4'
+    create_a_book
+    when '5'
+    create_a_rental
+    when '6'
+    list_all_rentals_for_person_id
+    when '7'
+    puts 'Hope you enjoy using this app!'
+    exit 0
+   end
+  end
