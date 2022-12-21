28f874b4e1a0a008934b775531ef785e050364be
ecekpo <ememekpo@yahoo.com>
2022-12-21 12:01:16 +0100

    Add linters error

--- classroom.rb
+++ classroom.rb
@@ -1,13 +1,13 @@
 class Classroom
-    attr_accessor :label, :students
-  
-    def initialize(label)
-     @label = label
-     @students = []
-    end
-  
-    def student(students)
-      @students << students
-      students.classroom = self
-     end
-    end
+  attr_accessor :label, :students
+
+  def initialize(label)
+    @label = label
+    @students = []
+  end
+
+  def student(students)
+    @students << students
+    students.classroom = self
+  end
+end
--- person.rb
+++ person.rb
@@ -1,33 +1,33 @@
 require './nameable'
 
 class Person < Nameable
-    attr_reader :id
-    attr_accessor :name, :age, :rental
-  
-    def initialize(age, name: 'unknown', parent_permission: true)
-      super()
-  
-      @id = Random.rand(1..1000)
-      @name = name
-      @age = age
-      @parent_permission = parent_permission
-      @rental = []
-    end
-  
-    def can_use_services?
-      of_age? || @parent_permission == true
-    end
-  
-    def of_age?
-      @age >= 18
-    end
-  
-    def correct_name
-      @name
-    end
-  
-    def rentals(rental)
-      @rental << rental
-      rental.person = self
-     end
+  attr_reader :id
+  attr_accessor :name, :age, :rental
+
+  def initialize(age, name: 'unknown', parent_permission: true)
+    super()
+
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+    @rental = []
   end
+
+  def can_use_services?
+    of_age? || @parent_permission == true
+  end
+
+  def of_age?
+    @age >= 18
+  end
+
+  def correct_name
+    @name
+  end
+
+  def rentals(rental)
+    @rental << rental
+    rental.person = self
+  end
+end
